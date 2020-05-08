3b0755c8f456e67524d11a64466f0ce74f2037f3
Georgy Komarov <jubnzv@gmail.com>
2020-05-08 19:38:29 +0300

    cfg_plotter: More representative pictures

--- src/python/plugins/cfg_plotter.py
+++ src/python/plugins/cfg_plotter.py
@@ -1,7 +1,7 @@
 from typing import List
 from pygraphviz import AGraph
 
-from .. import om
+from .. import om  # noqa
 
 
 class CFGPlotter:
@@ -11,9 +11,17 @@ class CFGPlotter:
         self.graph = self.generate_graph()
 
     def generate_graph(self) -> AGraph:
-        graph = AGraph(directed=True)
+        graph = AGraph(directed=True, splines='curved', overlap='vpsc')
         for cfg in self.cfgs:
             for bb in cfg.basic_blocks:
+                style = {}
+                if bb.type == 'BBExit':
+                    style = dict(style='filled', color='#665c54')
+                if bb.type == 'BBEntry':
+                    style = dict(style='filled', color='#458588')
+                graph.add_node(
+                    n=bb.id, label=f'bb={bb.id} stmt={bb.stmt_id}', **style)
+
                 for succ in bb.succs:
                     graph.add_edge(bb.id, succ)
                 for pred in bb.preds:
