87963063aa014d181b978496c7679d92e2aa9b33
Georgy Komarov <jubnzv@gmail.com>
2020-05-23 06:24:07 +0300

    Increase McCabe threshold

--- src/core/config.ml
+++ src/core/config.ml
@@ -7,7 +7,7 @@
 let max_string_len = 4096
 
 (** Threshold of McCabe complexity to generate warnings. *)
-let mccabe_complexity_threshold = 20
+let mccabe_complexity_threshold = 15
 
 (** Threshold of maximum number of statements in POU to generate warnings. *)
 let statements_num_threshold = 25
