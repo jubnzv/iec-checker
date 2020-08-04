83b1349187eed1e987634455172724884950584b
Georgiy Komarov <jubnzv@gmail.com>
2020-08-04 20:17:36 +0300

    feature: Support SEL XML format
    Add initial support of the Schweitzer Engineering Laboratories XML
    Format. The implemented functionality allows to reconstruct the ST
    programs from XML files in the directory on the given paths.
    
    See: #6

--- src/bin/iec_checker.ml
+++ src/bin/iec_checker.ml
@@ -11,10 +11,13 @@ module WO = Warn_output
 
 (** Format of files given to checker *)
 type input_format_ty =
-  | InputST (** StructuredText source code *)
-  | InputXML (** PLCOpen schemas *)
+  | InputST     (** Structured Text source code *)
+  | InputXML    (** PLCOpen schemas *)
+  | InputSELXML (** Schweitzer Engineering Laboratories XML Format*)
 
-let parse_with_error (lexbuf: Lexing.lexbuf) : (S.iec_library_element list * Warn.t list) =
+type parse_results = S.iec_library_element list * Warn.t list
+
+let parse_with_error (lexbuf: Lexing.lexbuf) : parse_results =
   let tokinfo lexbuf = TI.create lexbuf in
   let l = Lexer.initial tokinfo in
   try (Parser.main l lexbuf), [] with
@@ -25,16 +28,16 @@ let parse_with_error (lexbuf: Lexing.lexbuf) : (S.iec_library_element list * War
   | e ->
     [], [(W.mk_from_lexbuf lexbuf "UnknownError" (Exn.to_string e))]
 
-let parse_stdin () : (S.iec_library_element list * Warn.t list) =
+let parse_stdin () : parse_results option =
   match In_channel.input_line stdin with
-  | None -> ([], [W.mk_internal ~id:"Cancel" ""])
-  | Some code -> (
+  | None -> None
+  | Some code -> begin
       let lexbuf = Lexing.from_string code in
       lexbuf.lex_curr_p <- { lexbuf.lex_curr_p with pos_fname = "stdin" };
-      let (elements, warns) = parse_with_error lexbuf in
-      (elements, warns))
+      Some(parse_with_error lexbuf)
+    end
 
-let parse_st_file (filename : string) : (S.iec_library_element list * Warn.t list) =
+let parse_st_file (filename : string) : parse_results =
   let inx = In_channel.create filename in
   let lexbuf = Lexing.from_channel inx in
   lexbuf.lex_curr_p <- { lexbuf.lex_curr_p with pos_fname = filename };
@@ -42,7 +45,7 @@ let parse_st_file (filename : string) : (S.iec_library_element list * Warn.t lis
   In_channel.close inx;
   (elements, warns)
 
-let parse_xml_file (filename : string) : (S.iec_library_element list * Warn.t list) =
+let parse_xml_file (filename : string) : parse_results =
   let inx = In_channel.create filename in
   let program = Plcopen.reconstruct_from_channel inx in
   In_channel.close inx;
@@ -51,44 +54,104 @@ let parse_xml_file (filename : string) : (S.iec_library_element list * Warn.t li
   let (elements, warns) = parse_with_error lexbuf in
   (elements, warns)
 
-(** Run the checker and return the error code. *)
-let run_checker filename in_fmt out_fmt create_dumps verbose interactive =
-  let (read_stdin : bool) = (String.equal "-" filename) || (String.is_empty filename) in
-  if (not (Sys.file_exists filename) && not read_stdin) then
-    let err = W.mk_internal ~id:"FileNotFoundError" (Printf.sprintf "File %s doesn't exists" filename) in
+(** Parses an XML file located on [filepath]. If the
+    file contains the valid XML, it returns parse results, otherwise None. *)
+let parse_sel_xml_file (filepath : string) : parse_results option =
+  let inx = In_channel.create filepath in
+  let program_opt = Sel.reconstruct_from_channel_opt inx in
+  In_channel.close inx;
+  match program_opt with
+  | Some(program) -> begin
+      let lexbuf = Lexing.from_string program in
+      lexbuf.lex_curr_p <- { lexbuf.lex_curr_p with pos_fname = filepath };
+      Some(parse_with_error lexbuf)
+    end
+  | None -> None
+
+let endswith s1 s2 =
+  let len1 = String.length s1 and len2 = String.length s2 in
+  if len1 < len2 then false
+  else
+    let sub = String.sub s1 ~pos:(len1 - len2) ~len:(len2) in
+    String.equal sub s2
+
+(** Returns a list of the files to be checked. *)
+let get_files_to_check paths in_fmt =
+  (** Recursively traverses [path] and returns absolute paths to the files with
+      the given [suffix]. *)
+  let walkthrough_directory path suffix =
+    let rec aux result = function
+      | f::fs when Sys.is_directory f -> begin
+          Caml.Sys.readdir f
+          |> Array.to_list
+          |> List.map ~f:(Filename.concat f)
+          |> List.filter ~f:(fun n -> endswith n suffix)
+          |> List.append fs
+          |> aux result
+        end
+      | f::fs -> aux (f::result) fs
+      | []    -> result
+    in
+    aux [] [path]
+  in
+  let suffix = match in_fmt with
+    | InputST                -> ".st"
+    | InputXML | InputSELXML -> ".xml"
+  in
+  List.fold_left
+    paths
+    ~init:[]
+    ~f:(fun acc p -> acc @ walkthrough_directory p suffix)
+
+(** Starts the iec-checker REPL. *)
+let start_repl interactive =
+  if interactive then Printf.printf "> ";
+  flush stdout;
+  parse_stdin ()
+
+(** Parse the file with the given path. *)
+let parse_file path in_fmt verbose : parse_results option =
+  if verbose then Printf.printf "Parsing %s ...\n" path;
+  match in_fmt with
+  | InputST -> Some(parse_st_file path)
+  | InputXML -> Some(parse_xml_file path)
+  | InputSELXML -> begin
+      parse_sel_xml_file path
+    end
+
+(** Runs the checker on the file with [path] and returns the error code. *)
+let run_checker path in_fmt out_fmt create_dumps verbose interactive =
+  let (read_stdin : bool) = (String.equal "-" path) || (String.is_empty path) in
+  if (not (Sys.file_exists path) && not read_stdin) then
+    let err = W.mk_internal ~id:"FileNotFoundError" (Printf.sprintf "File %s doesn't exists" path) in
     WO.print_report [err] out_fmt;
     127
   else
-    let (elements, parser_warns) =
-      if read_stdin then begin
-        if interactive then Printf.printf "> ";
-        flush stdout;
-        parse_stdin ()
-      end else begin
-        if verbose then Printf.printf "Parsing %s ...\n" filename;
-        match in_fmt with
-        | InputST -> parse_st_file filename
-        | InputXML -> parse_xml_file filename
-      end
+    let results_opt =
+      if read_stdin then start_repl interactive
+      else parse_file path in_fmt verbose
     in
-    let envs = Ast_util.create_envs elements in
-    let cfgs = Cfg.create_cfgs elements in
-    if create_dumps then
-      Dump.create_dump elements envs cfgs
-        (if read_stdin then "stdin" else filename);
-    let decl_warns = Declaration_analysis.run elements envs in
-    let unused_warns = Unused_variable.run elements in
-    let ud_warns = Use_define.run elements in
-    let lib_warns = Lib.run_all_checks elements envs cfgs (not verbose) in
-    WO.print_report (
-      parser_warns @
-      decl_warns @
-      unused_warns @
-      ud_warns @
-      lib_warns)
-      out_fmt;
-
-    if not (List.is_empty parser_warns) then 1 else 0
+    match results_opt with
+    | None -> 0
+    | Some(elements, parser_warns) -> begin
+        let envs = Ast_util.create_envs elements in
+        let cfgs = Cfg.create_cfgs elements in
+        if create_dumps then
+          Dump.create_dump elements envs cfgs
+            (if read_stdin then "stdin" else path);
+        let decl_warns = Declaration_analysis.run elements envs in
+        let unused_warns = Unused_variable.run elements in
+        let ud_warns = Use_define.run elements in
+        let lib_warns = Lib.run_all_checks elements envs cfgs (not verbose) in
+        WO.print_report (
+          parser_warns @
+          decl_warns @
+          unused_warns @
+          ud_warns @
+          lib_warns)
+          out_fmt;
+        if not (List.is_empty parser_warns) then 1 else 0
+      end
 
 let () =
   Clap.description "Static analysis of IEC 61131-3 programs ";
@@ -98,15 +161,20 @@ let () =
       ~short: 'i'
       ~long: "input-format"
       ~description:
-        "Format of the input files. Supported formats: 'st' and 'xml'."
+        "Format of the input files. Supported formats:
+          + st - Structured Text source
+          + xml - PLCOpen XML
+          + selxml - Schweitzer Engineering Laboratories XML"
       ~placeholder: "INPUT_FORMAT"
       "st"
   in
   let input_format = match in_str with
     | s when String.equal "st" s -> InputST
     | s when String.equal "xml" s -> InputXML
+    | s when String.equal "selxml" s -> InputSELXML
     | s -> begin
-        Printf.eprintf "Unknown input format '%s'. Supported: 'st and 'xml.\n" s;
+        Printf.eprintf "Unknown input format '%s'.\n" s;
+        Printf.eprintf "Avaialble formats: 'st', 'xml' and 'selxml'\n";
         exit 1
       end
   in
@@ -159,17 +227,17 @@ let () =
       false
   in
 
-  let files =
+  let paths =
     Clap.list_string
       ~description:
-        "List of source files to check."
-      ~placeholder: "FILE"
+        "Paths to source files or directories to check."
+      ~placeholder: "PATHS"
       ()
   in
 
   Clap.close ();
 
-  if List.is_empty files then begin
+  if List.is_empty paths then begin
     Printf.eprintf "No input files!\n\n";
     Clap.help ();
     exit 1
@@ -177,9 +245,10 @@ let () =
 
   else if phys_equal 0
       begin
-        List.fold_left files
-          ~f:(fun return_codes f -> return_codes @ [run_checker f input_format output_format d v i])
-          ~init:[]
+        get_files_to_check paths input_format
+        |> List.fold_left
+            ~f:(fun return_codes f -> return_codes @ [run_checker f input_format output_format d v i])
+            ~init:[]
         |> List.filter ~f:(fun rc -> not (phys_equal rc 0))
         |> List.length
       end
---
+++ src/core/sel.ml
@@ -0,0 +1,84 @@
+open Core_kernel
+module S = Syntax
+
+type pou_ty =
+  | FB
+  | Function
+  | Program
+
+type parse_result = {
+  mutable interface: string option;
+  mutable implementation: string option;
+  mutable pou_type: pou_ty option;
+}
+
+(** Parses the text string from the XMLM node. *)
+let pull_data i =
+  if Xmlm.eoi i then None
+  else
+    match Xmlm.input i with
+    | `Data d -> Some(d)
+    | _ -> None
+
+let pull_pou_type i =
+  if Xmlm.eoi i then None
+  else
+    match Xmlm.input i with
+    | `Data d -> begin
+        if (String.equal d "FunctionBlock") then
+          Some(FB)
+        else if (String.equal d "Function") then
+          Some(Function)
+        else if (String.equal d "Program") then
+          Some(Program)
+        else
+          None
+      end
+    | _ -> None
+let get_end_tag = function
+  | FB -> "END_FUNCTION_BLOCK"
+  | Function -> "END_FUNCTION"
+  | Program -> "END_PROGRAM"
+
+(** Creates a source code from the parse_result entry and returns the source code. *)
+let reconstruct_source res : (string option) =
+  match res.interface, res.implementation, res.pou_type with
+  | Some(interface), Some(impl), Some(pou_type) ->
+    Some(String.concat ~sep:"\n" [interface; impl; (get_end_tag pou_type)])
+  | _  -> None
+
+
+(** Iterate over all XML elements in schema to parse their source code. *)
+let rec parse_source i d acc =
+  if Xmlm.eoi i then reconstruct_source acc
+  else
+    match Xmlm.input i with
+    |`El_start ((_, tag), _) when (String.equal tag "Interface") -> begin
+        acc.interface <- (pull_data i);
+        parse_source i d acc
+      end
+    |`El_start ((_, tag), _) when (String.equal tag "Implementation") -> begin
+        acc.implementation <- (pull_data i);
+        parse_source i d acc
+      end
+    |`El_start ((_, tag), _) when (String.equal tag "POUKind") -> begin
+        acc.pou_type <- (pull_pou_type i);
+        parse_source i d acc
+      end
+    |`El_start _ -> parse_source i (d + 1) acc
+    | `El_end -> if (phys_equal d 1) then reconstruct_source acc else parse_source i (d - 1) acc
+    | _ -> parse_source i d acc
+
+let reconstruct_from_channel_opt ic =
+  let i_opt = try Some(Xmlm.make_input (`Channel ic)) with
+    | _ -> None
+  in
+  match i_opt with
+  | None -> None
+  | Some(i) -> begin
+      let result = { interface = None;
+                     implementation = None;
+                     pou_type = None } in
+      try (parse_source i 1 result) with
+      | _ -> None
+    end
