951adcef020c4faacffc88d693560a789af6f0f4
Georgy Komarov <jubnzv@gmail.com>
2021-02-13 11:12:28 +0300

    cp3: Fix false positive for direct variables

--- src/lib/plcopen_cp3.ml
+++ src/lib/plcopen_cp3.ml
@@ -9,11 +9,15 @@ let do_check elems =
   |> List.fold_left
     ~init:[]
     ~f:(fun acc decl -> begin
-          if phys_equal (S.VarDecl.get_was_init decl) false then
-            let ti = S.VarDecl.get_var_ti decl in
-            let msg = Printf.sprintf("Variable %s shall be initialized before being used") @@ S.VarDecl.get_var_name decl in
-            let w = Warn.mk ti.linenr ti.col "PLCOPEN-CP3" msg in
-            acc @ [w];
-          else
-            acc;
+          match S.VarDecl.get_located_at decl with
+          | None -> begin
+              if phys_equal (S.VarDecl.get_was_init decl) false then
+                let ti = S.VarDecl.get_var_ti decl in
+                let msg = Printf.sprintf("Variable %s shall be initialized before being used") @@ S.VarDecl.get_var_name decl in
+                let w = Warn.mk ti.linenr ti.col "PLCOPEN-CP3" msg in
+                acc @ [w]
+              else
+                acc
+            end
+          | _ -> acc
         end)
