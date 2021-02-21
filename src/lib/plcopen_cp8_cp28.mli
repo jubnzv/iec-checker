129ab7945a83bac0c488388bfac7810446a9956c
Georgy Komarov <jubnzv@gmail.com>
2021-02-21 20:16:06 +0300

    Typos, fixes

--- src/lib/plcopen_cp8_cp28.mli
+++ src/lib/plcopen_cp8_cp28.mli
@@ -1,5 +1,5 @@
 (** PLCOPEN-CP8: Floating point comparison shall not be equality or inequality
-    PLCOPEN-CP28: Time and physical measures comparissons shall not be equality or inequality *)
+    PLCOPEN-CP28: Time and physical measures comparisons shall not be equality or inequality *)
 open IECCheckerCore
 module S = Syntax
 val do_check : S.iec_library_element list -> Warn.t list
