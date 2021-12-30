e6595bea52d8fc6dfe0d2e8b4012af8b21a7aa55
Georgy Komarov <jubnzv@gmail.com>
2021-12-30 06:55:29 +0300

    python: Remove extra prints

--- src/python/core.py
+++ src/python/core.py
@@ -55,7 +55,6 @@ def run_checker_full_out(file_path: str, binary: str = binary_default,
                 *args) -> Tuple[int, str]:
     """Run iec-checker core for a given file and capture the whole output.
     No extra options will be set by default."""
-    print([binary, *args, file_path])
     p = subprocess.Popen([binary, *args, file_path],
                          stdout=subprocess.PIPE,
                          stderr=subprocess.STDOUT)
